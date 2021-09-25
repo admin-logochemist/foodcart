b3adb195d7795a11afbb2e6df81834e808e06fa8
admin-logochemist <team@logochemist.com>
2021-09-25 05:04:36 +0500

    SignUp

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth,GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +20,6 @@ const firebaseConfig = {
 const app= initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const projstore=getStorage(app)
 const provider = new GoogleAuthProvider();
-export  {db,auth,provider};
+export  {db,auth,provider,projstore};
--- src/pages/AddResturant.js
+++ src/pages/AddResturant.js
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Button } from '@material-ui/core'
+import "./AddResturant.css";
+import { collection, addDoc } from "firebase/firestore"; 
+import { db, projstore} from "../firebase";
 function AddResturant() {
+    const [email, setEmail] = useState("");
+    const [ address,setAddress ] = useState("");
+    const [resName, setResName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [cusine, setCusine] = useState("");
+    const [file, setFile] = useState("");
+    const [url, setURL] = useState("");
+    const addImagetoPost = function (e) {
+        setFile(e.target.files[0]);
+    
+      };
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        console.log(file)
+    
+    
+        const docRef = addDoc(collection(db, "resturant"),(
+          {
+            resName: resName,
+            phone: phone,
+            email: email,
+            cusine: cusine,
+            
+          }
+        )
+      
+
+        )}
+    
     return (
         <div className="AddResturant">
+            <div className="resturant__container">
             <h1>Add Resturant</h1>
+            <form>
+
+<input value={resName} type="text" placeholder="Restaurant Name" onChange={(e)=>setResName(e.target.value)}/>
+<input value={phone} type="text" placeholder="Phone"onChange={(e)=>setPhone(e.target.value)}/>
+<input value={email} type="email" placeholder="Email"onChange={(e)=>setEmail(e.target.value)}/>
+<input value={address} type="text" placeholder="Address" onChange={(e)=>setAddress(e.target.value)}/>
+<input value={cusine} type="text" placeholder="Cusine" onChange={(e)=>setCusine(e.target.value)}/>
+<label for="file">CoverImage</label>
+<input onChange={addImagetoPost} type="file" />
+ 
+
+
+<Button  onClick={handleSubmit}>LOGIN</Button>
+
+
+</form>
+</div>
         </div>
     )
 }
